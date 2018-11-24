eb821a2369ac8bee27f6c832780e746e95bd14d4
Ahmet <38635158+ahmt9417@users.noreply.github.com>
2018-11-24 22:18:49 +0300

    Update 1-Data_Preprocessing.py

--- 1-Data_Preprocessing.py
+++ 1-Data_Preprocessing.py
@@ -11,43 +11,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 data = pd.read_csv("dataset.csv")
 
 height = data[["height"]]
+print(height)
 height_weight = data[["height","weight"]]
+print(height_weight)
 
-#%%missing values
+#%%missing values filling with column's mean
 from sklearn.preprocessing import Imputer
 imputer= Imputer(missing_values="NaN", strategy = "mean", axis=0 )    
 
 numbers = data.iloc[:,1:4].values
 numbers = imputer.fit_transform(numbers)
-#%%encoder:  categorical to numeric
 
+#%%encoder:  categorical to numeric
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-le = LabelEncoder()
-ohe = OneHotEncoder(categorical_features="all")
+le = LabelEncoder() #give numbers to features
+ohe = OneHotEncoder(categorical_features="all") 
 
 country = data.iloc[:,0:1].values
 country[:,0] = le.fit_transform(country[:,0])
 country = ohe.fit_transform(country).toarray()
 
-#%%dataframe
+#%%make dataframe
 country_df = pd.DataFrame(data = country, index = range(22), 
                      columns=["fr","tr","us"] )
-
 
 numbers_df = pd.DataFrame(data = numbers, index = range(22),
                      columns = ["height","weight","age"])
 
-
 gender = data.iloc[:,-1].values
-
-
 gender_df = pd.DataFrame(data = gender , index=range(22), columns=["gender"])
 
-#%%concating
+#%%concatenating
 data_new = pd.concat([country_df,numbers_df],axis=1)
 data_all = pd.concat([data_new,gender_df],axis=1)
 
